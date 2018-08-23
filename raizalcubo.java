e3ddd45d1ae19aa5b96b59bd527acce18c6b5479
David Andres Alcazar Escobar <alcazarescobar2006@gmail.com>
2018-08-22 20:01:49 -0600

    Se agrego raiz al cubo

---
+++ raizalcubo.java
@@ -0,0 +1,6 @@
+int n, base;
+System.out.println("Ingrese la base: ");
+base = tec.nextInt();
+n = 3;
+int resultado = (int) Math.pow(base, n);
+System.out.println(base + " elevado a la " + n + " es igual a " + resultado);
